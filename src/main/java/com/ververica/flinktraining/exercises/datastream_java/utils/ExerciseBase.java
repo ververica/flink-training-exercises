7b57ee6e1c83047ae2a33365fdc3152af733aacb
Ativ Aggarwal <ativ97@gmail.com>
2019-12-23 12:14:16 -0600

    Ride filter

--- src/main/java/com/ververica/flinktraining/exercises/datastream_java/utils/ExerciseBase.java
+++ src/main/java/com/ververica/flinktraining/exercises/datastream_java/utils/ExerciseBase.java
@@ -28,8 +28,8 @@ public class ExerciseBase {
 	public static SinkFunction out = null;
 	public static int parallelism = 4;
 
-	public final static String pathToRideData = "/Users/david/stuff/flink-training/trainingData/nycTaxiRides.gz";
-	public final static String pathToFareData = "/Users/david/stuff/flink-training/trainingData/nycTaxiFares.gz";
+	public final static String pathToRideData = "C:/dev/flink-training-exercises/nycTaxiRides.gz";
+	public final static String pathToFareData = "C:/dev/flink-training-exercises/nycTaxiFares.gz";
 
 	public static SourceFunction<TaxiRide> rideSourceOrTest(SourceFunction<TaxiRide> source) {
 		if (rides == null) {
--- src/main/scala/com/ververica/flinktraining/exercises/datastream_scala/basics/RideCleansingExercise.scala
+++ src/main/scala/com/ververica/flinktraining/exercises/datastream_scala/basics/RideCleansingExercise.scala
@@ -18,22 +18,26 @@ package com.ververica.flinktraining.exercises.datastream_scala.basics
 
 import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiRideSource
 import com.ververica.flinktraining.exercises.datastream_java.utils.ExerciseBase._
-import com.ververica.flinktraining.exercises.datastream_java.utils.{ExerciseBase, GeoUtils, MissingSolutionException}
+import com.ververica.flinktraining.exercises.datastream_java.utils.{
+  ExerciseBase,
+  GeoUtils,
+  MissingSolutionException
+}
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala._
 
 /**
- * The "Ride Cleansing" exercise of the Flink training
- * (http://training.ververica.com).
- *
- * The task of the exercise is to filter a data stream of taxi ride records to keep only rides that
- * start and end within New York City. The resulting stream should be printed to the
- * standard out.
- *
- * Parameters:
- * -input path-to-input-file
- *
- */
+  * The "Ride Cleansing" exercise of the Flink training
+  * (http://training.ververica.com).
+  *
+  * The task of the exercise is to filter a data stream of taxi ride records to keep only rides that
+  * start and end within New York City. The resulting stream should be printed to the
+  * standard out.
+  *
+  * Parameters:
+  * -input path-to-input-file
+  *
+  */
 object RideCleansingExercise extends ExerciseBase {
   def main(args: Array[String]) {
     // parse parameters
@@ -41,18 +45,24 @@ object RideCleansingExercise extends ExerciseBase {
     val input = params.get("input", ExerciseBase.pathToRideData)
 
     val maxDelay = 60 // events are out of order by max 60 seconds
-    val speed = 600   // events of 10 minutes are served in 1 second
+    val speed = 600 // events of 10 minutes are served in 1 second
 
     // set up the execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(parallelism)
 
     // get the taxi ride data stream
-    val rides = env.addSource(rideSourceOrTest(new TaxiRideSource(input, maxDelay, speed)))
+    val rides = env.addSource(
+      rideSourceOrTest(new TaxiRideSource(input, maxDelay, speed))
+    )
 
-    val filteredRides = rides
-      // filter out rides that do not start and end in NYC
-      .filter(ride => throw new MissingSolutionException)
+    val filteredRides = rides.filter(
+      r =>
+        (GeoUtils.isInNYC(r.startLon, r.startLat) && GeoUtils
+          .isInNYC(r.endLon, r.endLat))
+    )
+    // filter out rides that do not start and end in NYC
+    //.filter(ride => throw new MissingSolutionException)
 
     // print the filtered stream
     printOrTest(filteredRides)
